825b566ad4d755f703f5e20ddf8324d658b9a628
kendra ash & alisa colon <student@epicodus.com>
2015-09-08 12:07:08 -0700

    add passing specs for determining the type of triangle

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,17 @@
 var triangleTracker = function(a, b, c) {
   if ((a !== null || b !== null || c !== null) && (a + b > c && b + c > a && c + a > b)) {
     return true;
-  } else {
+    if (a===b && b===c) {
+      return true;
+    } else if (a===b || b===c || a===c) {
+        return true;
+    } else {
+      return true
+    }
+  }
+
+
+  else {
     return false;
   }
 };
--- spec/specs.js
+++ spec/specs.js
@@ -6,4 +6,16 @@ describe('triangleTracker', function() {
   it("is true if a valid triangle", function() {
     expect(triangleTracker(5, 4, 8)).to.equal(true);
   });
+
+  it("is equilateral triangle", function() {
+    expect(triangleTracker(5,5,5)).to.equal(true);
+  });
+
+  it("is isosceles triangle", function() {
+    expect(triangleTracker(5,5,4)).to.equal(true);
+  });
+
+  it("is scalene triangle", function() {
+    expect(triangleTracker(5,6,4)).to.equal(true);
+  });
 });
