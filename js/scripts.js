26ea8a21757f42ea4298c12a83dfcff8bea1df9f
kendra ash & alisa colon <student@epicodus.com>
2015-09-08 11:46:56 -0700

    add passing spec and function to find if a triangle exists

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+var triangleTracker = function(a, b, c) {
+  if(a === null || b === null || c === null) {
+    return false;
+  } else {
+    return true;
+  }
+};
---
+++ spec/specs.js
@@ -0,0 +1,5 @@
+describe('triangleTracker', function() {
+  it("is true if three sides exist", function() {
+    expect(triangleTracker(1,2,3)).to.equal(true);  
+  });
+});
